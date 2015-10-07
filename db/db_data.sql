3067174983ba4730950596fe3c7e5437eb02fb03
eugenbk <eugenbk@mail.ru>
2015-10-07 19:33:12 +0200

    db script updated: photo added

--- db/db_data.sql
+++ db/db_data.sql
@@ -13,3 +13,10 @@ INSERT INTO `T_Photo` SET
 `photo_path` = 'DSC_3830.jpg',
 `upload_date` = '2015-10-06',
 `photo_date` = '2015-07-18';
+
+INSERT INTO `T_Photo` SET
+`id` = 3,
+`thumb_path` = 'DSC_5177_thumb.jpg',
+`photo_path` = 'DSC_5177.jpg',
+`upload_date` = '2015-10-07',
+`photo_date` = '2015-09-26';
